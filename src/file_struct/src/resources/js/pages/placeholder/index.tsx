5f472826371b8670eebbf4ab68e42e48e7bf6119
Joshua <joshuaheathcote1987@gmail.com>
2025-09-07 18:20:30 +0700

    Unable to add image, try again later

--- src/file_struct/src/resources/js/pages/placeholder/index.tsx
+++ src/file_struct/src/resources/js/pages/placeholder/index.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import Logo from '../../../../../logo.png';
 
 export default function Placeholder() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <img src={Logo} alt="Hotswap Logo" className="w-64 h-64 mb-8" />
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <div className="text-4xl text-red-600">Hotswap</div>
     </div>
   );
 }
